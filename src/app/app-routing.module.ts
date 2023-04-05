56d504438ba2dd2524e780960351e348eb659273
Angular0527 <Angular0527@outlook.com>
2023-04-05 18:57:55 +0200

    added init blog page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BlogComponent} from "./blog/blog/blog.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'blog', pathMatch: 'full'},
+  {path: 'blog', component: BlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent  {
+  title= 'Angular15 Item Manage Page';
 }
